4987184ca6cb1dab54739bf76371e66c41f4ea48
Vladyslav Protsenko <vladyslav.protsenko@ucu.edu.ua>
2021-11-29 15:10:58 +0200

    added gitignore

--- src/main/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysis.java
+++ src/main/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysis.java
@@ -1,37 +1,57 @@
 package ua.edu.ucu.tempseries;
 
+import java.util.Arrays;
+
 public class TemperatureSeriesAnalysis {
+    private double[] temperatureSeries;
 
     public TemperatureSeriesAnalysis() {
 
     }
 
     public TemperatureSeriesAnalysis(double[] temperatureSeries) {
-
+        this.temperatureSeries = Arrays.copyOf(temperatureSeries, temperatureSeries.length);
     }
 
-    public double average() {
-        return -1;
+    public double average() throws IllegalArgumentException{
+        double sum = 0;
+        for (int i=0; i<temperatureSeries.length; i++){
+            sum += temperatureSeries[i];
+        }
+        return sum/temperatureSeries.length;
     }
 
-    public double deviation() {
-        return 0;
+    public double deviation() throws IllegalArgumentException{
+        double mean = average();
+        double variance = 0;
+        for (int i=0; i<temperatureSeries.length; i++){
+            variance += Math.pow(temperatureSeries[i]-mean, 2);
+        }
+        return Math.sqrt(variance);
     }
 
-    public double min() {
-        return 0;
+    public double min() throws IllegalArgumentException{
+        return findTempClosestToValue(-1000);
     }
 
-    public double max() {
-        return 0;
+    public double max() throws IllegalArgumentException{
+        return findTempClosestToValue(Double.POSITIVE_INFINITY);
     }
 
     public double findTempClosestToZero() {
-        return 0;
+        return findTempClosestToValue(0);
     }
 
-    public double findTempClosestToValue(double tempValue) {
-        return 0;
+    public double findTempClosestToValue(double tempValue) throws IllegalArgumentException{
+        double diff = Double.POSITIVE_INFINITY;
+        double closestTemp = 0;
+        for (double temp: temperatureSeries){
+            if (Math.abs(temp - tempValue) < diff){
+                closestTemp = temp;
+                diff = Math.abs(temp - tempValue);
+            }
+        }
+        return closestTemp;
     }
 
     public double[] findTempsLessThen(double tempValue) {
